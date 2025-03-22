78ed99f18d05449affc569e12b660dd064985c4c
idityaGE <am44910606@gmail.com>
2025-03-22 07:54:15 +0530

    Refactor number-to-words conversion logic in Q6; add Q7 for counting word occurrences in a paragraph

--- OOPS-Assignment-5/Q6.java
+++ OOPS-Assignment-5/Q6.java
@@ -1,93 +1,54 @@
 
 class Q6 {
 
-    private static int countDigit(long num) {
-        int cnt = 0;
-        while (num > 0) {
-            num /= 10;
-            cnt++;
-        }
-        return cnt;
-    }
-
     private static String getString(int num) {
         if (num >= 0 && num <= 9) {
-            switch (num) {
-                case 0:
-                    return "zero";
-                case 1:
-                    return "one";
-                case 2:
-                    return "two";
-                case 3:
-                    return "three";
-                case 4:
-                    return "four";
-                case 5:
-                    return "five";
-                case 6:
-                    return "six";
-                case 7:
-                    return "seven";
-                case 8:
-                    return "eight";
-                case 9:
-                    return "nine";
-                default:
-                    throw new AssertionError();
-            }
+            return new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}[num];
         } else if (num >= 11 && num <= 19) {
-            switch (num) {
-                case 11:
-                    return "eleven";
-                case 12:
-                    return "twelve";
-                case 13:
-                    return "thirteen";
-                case 14:
-                    return "fourteen";
-                case 15:
-                    return "fifteen";
-                case 16:
-                    return "sixteen";
-                case 17:
-                    return "seventeen";
-                case 18:
-                    return "eighteen";
-                case 19:
-                    return "nineteen";
-                default:
-                    throw new AssertionError();
-            }
+            return new String[]{"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}[num - 11];
         } else if (num % 10 == 0) {
-            switch (num) {
-                case 10:
-                    return "ten";
-                case 20:
-                    return "twenty";
-                case 30:
-                    return "thirty";
-                case 40:
-                    return "fourty";
-                case 50:
-                    return "fifty";
-                case 60:
-                    return "sixty";
-                case 70:
-                    return "seventy";
-                case 80:
-                    return "eighty";
-                case 90:
-                    return "ninty";
-                default:
-                    throw new AssertionError();
-            }
+            return new String[]{"ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty"}[num / 10 - 1];
         } else {
-			return "";
-		}
+            return getString(num - num % 10) + " " + getString(num % 10);
+        }
+    }
+
+    private static String convertToWords(long num) {
+        if (num == 0) return "zero";
+        
+
+        StringBuilder words = new StringBuilder();
+
+        if (num / 10000000 > 0) {
+            words.append(getString((int) (num / 10000000))).append(" crore ");
+            num %= 10000000;
+        }
+        if(num / 100000 > 0) {
+            words.append(getString((int) (num / 100000))).append(" lakh ");
+            num %= 100000;
+        }
+        if (num / 1000 > 0) {
+            words.append(getString((int) (num / 1000))).append(" thousand ");
+            num %= 1000;
+        }
+        if (num / 100 > 0) {
+            words.append(getString((int) (num / 100))).append(" hundred ");
+            num %= 100;
+        }
+        if (num > 0) {
+            if (num < 10 || (num >= 11 && num <= 19) || num % 10 == 0) {
+                words.append(getString((int) num));
+            } else {
+                words.append(getString((int) (num / 10) * 10)).append(" ").append(getString((int) (num % 10)));
+            }
+        }
+        String result = words.toString().trim();
+        return result.substring(0, 1).toUpperCase() + result.substring(1);
     }
 
     public static void main(String[] args) {
-
+        long number = 3021320;
+        String ans = convertToWords(number);
+        System.out.println(ans);
     }
 }
---
+++ OOPS-Assignment-5/Q7.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Q7 {
+
+    public static int countWordOccurrences(String text, String word) {
+        String[] words = text.split("\\s+|[.,!?;]");
+        int count = 0;
+
+        for (String w : words) {
+            if (w.equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the paragraph:");
+        String paragraph = sc.nextLine();
+
+        System.out.println("Enter the word to count:");
+        String word = sc.next();
+
+        int count = countWordOccurrences(paragraph, word);
+
+        System.out.println("The word '" + word + "' occurs " + count + " times in the paragraph.");
+    }
+}
